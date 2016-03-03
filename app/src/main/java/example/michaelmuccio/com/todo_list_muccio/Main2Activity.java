d5d320c556df818c891d80a826df2913c194f510
Michael Muccio <michaelmuccio@gmail.com>
2016-03-03 15:23:47 -0800

    new project repository

---
+++ app/src/main/java/example/michaelmuccio/com/todo_list_muccio/Main2Activity.java
@@ -0,0 +1,125 @@
+package example.michaelmuccio.com.todo_list_muccio;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+import java.util.LinkedList;
+
+
+public class Main2Activity extends AppCompatActivity {
+
+    public static final String THINGTODO = "details";
+    LinkedList<String> mDetails;
+    ArrayAdapter<String> mToDeatailsAdap;
+    EditText userDetailInput;
+    ListView usersDetails;
+    TextView titleChange;
+    TextView emptyDeats;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDetails = new LinkedList<>();
+        mToDeatailsAdap = new ArrayAdapter<String>(Main2Activity.this, android.R.layout.simple_list_item_1, mDetails);
+
+        usersDetails = (ListView) findViewById(R.id.userDetailList);
+
+        userDetailInput = (EditText) findViewById(R.id.userInputDetails);
+        usersDetails.setAdapter(mToDeatailsAdap);
+
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (userDetailInput.getText().toString().isEmpty()){
+                    Toast.makeText(Main2Activity.this, "Not a valid Detail", Toast.LENGTH_SHORT).show();
+                } else {
+                    mDetails.add(userDetailInput.getText().toString());
+                    mToDeatailsAdap.notifyDataSetChanged();
+                    userDetailInput.setText("");
+                    changeEmptyDetailsView();
+                }
+
+            }
+        });
+
+        changeTitle(getIntent().getStringExtra(THINGTODO));
+        strikeThrough();
+        longPressDelete();
+    }
+
+    private void  strikeThrough(){
+
+        usersDetails.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                TextView v = (TextView) view;
+
+                v.setPaintFlags(v.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+            }
+        });
+
+    }
+
+    private void longPressDelete(){
+
+        usersDetails.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                mDetails.remove(position);
+                mToDeatailsAdap.notifyDataSetChanged();
+                return true;
+
+            }
+        });
+
+    }
+
+    public String changeTitle(String title) {
+
+        titleChange = (TextView) findViewById(R.id.detailsTitle);
+
+        titleChange.setText(title);
+
+        return title;
+
+    }
+
+    public void changeEmptyDetailsView() {
+
+        emptyDeats = (TextView) findViewById(R.id.textView);
+
+        if (mDetails != null) {
+            Log.d("MainActivity2", "is null?");
+
+            emptyDeats.setText("My Details:");
+
+        }
+
+    }
+
+
+
+}
---
+++ app/src/main/java/example/michaelmuccio/com/todo_list_muccio/MainActivity.java
@@ -0,0 +1,176 @@
+package example.michaelmuccio.com.todo_list_muccio;
+
+import android.content.Intent;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+
+    LinkedList <String> mtoDoList;
+    ArrayAdapter <String> mtoDoListAdap;
+    EditText userToDoInput;
+    ListView usersList;
+    public static final int REQUEST_CODE = 16;
+    TextView emptyList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mtoDoList = new LinkedList<>();
+        mtoDoListAdap = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, mtoDoList);
+
+        usersList = (ListView) findViewById(R.id.userToDoList);
+
+        userToDoInput = (EditText) findViewById(R.id.userInput);
+        usersList.setAdapter(mtoDoListAdap);
+
+            FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    if(userToDoInput.getText().toString().isEmpty()){
+
+                        Toast.makeText(MainActivity.this,"Not a valid TO DO!", Toast.LENGTH_SHORT).show();
+                    }else {
+
+                        mtoDoList.add(userToDoInput.getText().toString());
+                        mtoDoListAdap.notifyDataSetChanged();
+                        userToDoInput.setText("");
+                        changeEmptyList();
+                    }
+
+                }
+            });
+
+
+        setOnClickListeners();
+        strikeThrough();
+        longPressDelete();
+
+    }
+
+    private void setOnClickListeners(){
+
+        final Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+
+        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                intent.putExtra("details", mtoDoList.get(position));
+                startActivityForResult(intent, 16);
+
+            }
+        });
+
+    }
+
+    public void changeEmptyList() {
+
+        emptyList = (TextView) findViewById(R.id.textView);
+
+        if (mtoDoList != null) {
+            Log.d("MainActivity2", "is null?");
+
+            emptyList.setText("My List:");
+
+        }
+    }
+
+    //add strike-through capability. Imported new widget and got code from StackOverflow
+    private void  strikeThrough(){
+
+        usersList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                TextView v = (TextView) view;
+
+                v.setPaintFlags(v.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+
+            }
+        });
+
+    }
+    //add longpress for deleting the List item completely
+    private void longPressDelete(){
+
+        //the actual listview
+        usersList.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                //need to reference the linkedlist and adaptor
+                mtoDoList.remove(position);
+                mtoDoListAdap.notifyDataSetChanged();
+                return true; //default was false
+
+            }
+        });
+
+    }
+
+    // add counter for items in list
+
+    private static int listItemCounter(int counter){
+
+        return counter;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == 16){
+            if(resultCode == RESULT_OK){
+
+            }
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+}
